02533b4249935241b315a8114c450d0406cf4fbe
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-01-26 16:06:06 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 #include <bn_sprite_text_generator.h>
 #include <bn_size.h>
 #include <bn_string.h>
-#include <bn_backdrop.h>
 
 #include "bn_sprite_items_dot.h"
 #include "bn_sprite_items_square.h"
@@ -27,12 +26,11 @@ static constexpr int MAX_Y = bn::display::height() / 2;
 static constexpr int MIN_X = -bn::display::width() / 2;
 static constexpr int MAX_X = bn::display::width() / 2;
 
-//new starting location for treasure and player 
-static constexpr int PLAYER_START_X = -50;
-static constexpr int PLAYER_START_Y = 50;
+// New starting positions stored as constant variables rather than literals
+static constexpr int PLAYER_START_X = -20;
+static constexpr int PLAYER_START_Y = 20;
 static constexpr int TREASURE_START_X = 0;
 static constexpr int TREASURE_START_Y = 0;
-
 
 // Number of characters required to show the longest numer possible in an int (-2147483647)
 static constexpr int MAX_SCORE_CHARS = 11;
@@ -56,9 +54,6 @@ int main()
     bn::sprite_ptr player = bn::sprite_items::square.create_sprite(PLAYER_START_X, PLAYER_START_Y);
     bn::sprite_ptr treasure = bn::sprite_items::dot.create_sprite(TREASURE_START_X, TREASURE_START_Y);
 
-    //Backdrop Color
-    bn::backdrop::set_color(bn::color(30, 0, 30));
-
     while (true)
     {
         // Move player with d-pad
@@ -77,14 +72,6 @@ int main()
         if (bn::keypad::down_held())
         {
             player.set_y(player.y() + SPEED);
-        }
-
-        if (bn::keypad::start_pressed())
-        {
-            player.set_x(PLAYER_START_X);
-            player.set_y(PLAYER_START_Y);
-            score = 0;
-            treasure.set_position(TREASURE_START_X, TREASURE_START_Y);
         }
 
         // The bounding boxes of the player and treasure, snapped to integer pixels
