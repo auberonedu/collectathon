a703f036e3e2f06b26fbe04126e1774840e092f8
Buay Dak <buaypd@gmail.com>
2026-01-28 20:53:57 +0000

    added the waves

--- src/main.cpp
+++ src/main.cpp
@@ -65,18 +65,22 @@ int main()
     bn::sprite_text_generator text_generator(common::fixed_8x16_sprite_font);
 
     int score = 0;
-    bn::vector<bn::sprite_ptr,5> score_label_sprites;
-    text_generator.generate(SCORE_X - 50 ,SCORE_Y, "SCORE:", score_label_sprites);
+    bn::vector<bn::sprite_ptr, 5> score_label_sprites;
+    text_generator.generate(SCORE_X - 50, SCORE_Y, "SCORE:", score_label_sprites);
 
     // This will hold the sprites for the speed boost count
     bn::vector<bn::sprite_ptr, 11> boost_label_sprites = {};
     bn::vector<bn::sprite_ptr, SPEED_BOOST_COUNT_CHARS> boost_sprites = {};
 
     text_generator.generate(SPEED_BOOST_COUNT_X - 90, SPEED_BOOST_COUNT_Y,
-    "BOOST LEFT:", boost_label_sprites);
+                            "BOOST LEFT:", boost_label_sprites);
 
     bn::sprite_ptr player = bn::sprite_items::square.create_sprite(PLAYER_START_X, PLAYER_START_Y);
     bn::sprite_ptr treasure = bn::sprite_items::dot.create_sprite(TREASURE_START_X, TREASURE_START_Y);
+
+    bn::vector<bn::sprite_ptr, 12> wave_sprites = {};
+    int wave_timer = 180;
+    int wave = 1;
 
     while (true)
     {
@@ -162,8 +166,8 @@ int main()
         {
             duration = 180;
             boostCount--;
-            if(boostCount < 0)
-            boostCount = 0;
+            if (boostCount < 0)
+                boostCount = 0;
         }
 
         // Update score display
@@ -181,10 +185,20 @@ int main()
                                 boost_sprites);
         // added Boost count text
 
-
         // Update RNG seed every frame so we don't get the same sequence of positions every time
         rng.update();
 
+        if (wave_timer > 0)
+        {
+            wave_sprites.clear();
+            bn::string<12> wave_string = "WAVE: " + bn::to_string<12>(wave);
+            text_generator.generate(-20, 0, wave_string, wave_sprites);
+            wave_timer--;
+        }
+        else
+        {
+            wave_sprites.clear();
+        }
         bn::core::update();
     }
 }
