2ba158e34bef839a615789ee35b6d7bba2860aa2
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-01-28 07:32:46 +0000

    Fix: player and treasure sprites

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 
-#include "bn_sprite_items_dot.h"
-#include "bn_sprite_items_square.h"
+#include "bn_sprite_items_oyster.h"
+#include "bn_sprite_items_crab.h"
 #include "bn_sound_items.h"
 #include "bn_sound.h"
 #include "common_fixed_8x16_font.h"
@@ -63,8 +63,8 @@ int main()
 
     int score = 0;
 
-    bn::sprite_ptr player = bn::sprite_items::square.create_sprite(PLAYER_START_X, PLAYER_START_Y);
-    bn::sprite_ptr treasure = bn::sprite_items::dot.create_sprite(TREASURE_START_X,
+    bn::sprite_ptr player = bn::sprite_items::crab.create_sprite(PLAYER_START_X, PLAYER_START_Y);
+    bn::sprite_ptr treasure = bn::sprite_items::oyster.create_sprite(TREASURE_START_X,
                                                                   TREASURE_START_Y);
 
     while (true)
