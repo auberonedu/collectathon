c14bc57a4779775ab8578ea50143ee52e0eded5e
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-02-04 00:03:16 +0000

    fixed boost bug

--- src/main.cpp
+++ src/main.cpp
@@ -95,6 +95,7 @@ int main()
         if(paused == true){
             SPEED = 0;
             ENEMY_SPEED = 0;
+            boost_amt = 0;
             paused_sprites.clear();
             instruction_sprites.clear();
             text_generator.generate(-75, 40, "Press START to play!", paused_sprites);
